114e3318d28c75c0077943ad3d19632426e90f75
Sami bentaiba <140959042+samibentaiba@users.noreply.github.com>
2025-05-08 15:27:03 +0100

    update to new portfolio

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { useContext } from 'react'
-import { ThemeContext } from './contexts/theme'
-import Header from './components/Header/Header'
-import About from './components/About/About'
-import Projects from './components/Projects/Projects'
-import Skills from './components/Skills/Skills'
-import ScrollToTop from './components/ScrollToTop/ScrollToTop'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
-import './App.css'
+import { useContext, useEffect } from 'react';
+import { ThemeContext } from './contexts/theme';
+import Header from './components/Header/Header';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Skills from './components/Skills/Skills';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import './App.css';
 
 const App = () => {
-  const [{ themeName }] = useContext(ThemeContext)
+  const [{ themeName }] = useContext(ThemeContext);
+
+  // Automatically redirect to the translated version of the site
+  useEffect(() => {
+    if (window.location.hostname === 'your-website.com' || window.location.hostname === 'www.your-website.com') {
+      window.location.replace('https://bentaidev.vercel.app/');  // Redirect to the translated version
+    }
+  }, []);
 
   return (
     <div id='top' className={`${themeName} app`}>
@@ -26,7 +33,7 @@ const App = () => {
       <ScrollToTop />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
